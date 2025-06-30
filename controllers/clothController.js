dedc40729102108cc84db69e42c631695ec8ab57
petraalpigiano <petraalpigiano@gmail.com>
2025-06-30 19:12:11 +0200

    add query on show

--- controllers/clothController.js
+++ controllers/clothController.js
@@ -2,15 +2,15 @@ import connection from "../data/db.js";
 
 // INDEX/HOMEPAGE
 // function homepage(req, res) {
-//   // const sqlMovies = `SELECT * FROM movies`;
-//   // connection.query(sqlMovies, (err, results) => {
+//   // const sqlClothes = `SELECT * FROM movies`;
+//   // connection.query(sqlClothes, (err, results) => {
 //   //   if (err)
 //   //     return res.status(500).json({
 //   //       error: "Richiesta fallita!",
 //   //     });
-//   //   results.map(function (currentMovie) {
-//   //     return (currentMovie.image =
-//   //       "http://localhost:3000/imgs/movies_cover/" + currentMovie.image);
+//   //   results.map(function (currentCloth) {
+//   //     return (currentCloth.image =
+//   //       "http://localhost:3000/imgs/movies_cover/" + currentCloth.image);
 //   //   });
 //   //   res.json(results);
 //   // });
@@ -18,6 +18,7 @@ import connection from "../data/db.js";
 
 // INDEX/CLOTHES LIST
 function index(req, res) {
+  // ex LISTA DI TUTTI I VESTITI
   const sqlClothes = `SELECT * FROM clothes`;
   connection.query(sqlClothes, (err, results) => {
     if (err)
@@ -33,34 +34,58 @@ function index(req, res) {
 }
 // SHOW/ CLOTH DETAILS
 function show(req, res) {
-  // const id = parseInt(req.params.id);
-  // const sqlMovies = `SELECT * FROM movies WHERE movies.id =? `;
-  // const sqlReviews = `SELECT name, vote, text, reviews.created_at, reviews.updated_at FROM movies INNER JOIN reviews ON movies.id = reviews.movie_id WHERE movies.id = ? `;
+  const id = parseInt(req.params.id);
+  // ex QUERY PER VESTITO SPECIFICO
+  const sqlClothes = `SELECT * FROM clothes WHERE clothes.id =? `;
+  // ex QUERY PER TAGLIE
+  const sqlSizes = `SELECT sizes.name
+FROM clothes
+INNER JOIN clothes_sizes
+ON clothes.id = clothes_sizes.cloth_id
+INNER JOIN sizes
+ON clothes_sizes.size_id = sizes.id
+WHERE clothes.id = ?`;
 
-  // connection.query(sqlMovies, [id], (err, results) => {
-  //   if (err)
-  //     return res.status(500).json({
-  //       error: "Richiesta fallita!",
-  //     });
-  //   if (results.length === 0) {
-  //     return res.status(404).json({ error: "Film non trovato!" });
-  //   }
-  //   results.map(function (currentMovie) {
-  //     return (currentMovie.image =
-  //       "http://localhost:3000/imgs/movies_cover/" + currentMovie.image);
-  //   });
-  //   const movie = results[0];
+  // ex QUERY PER CATEGORIA
+  const sqlCategory = `
+SELECT categories.name
+FROM clothes
+INNER JOIN categories
+ON clothes.categories_id = categories.id
+WHERE clothes.id = ?`;
 
-  //   connection.query(sqlReviews, [id], (err, results) => {
-  //     if (err)
-  //       return res.status(500).json({
-  //         error: "Richiesta fallita!",
-  //       });
-  //     movie.review = results;
-  //     res.json(movie);
-  //   });
-  // });
-  res.send("show funziona");
+  // ex DETTAGLIO VESTITO SPECIFICO
+  connection.query(sqlClothes, [id], (err, results) => {
+    if (err)
+      return res.status(500).json({
+        error: "Richiesta fallita!",
+      });
+    if (results.length === 0) {
+      return res.status(404).json({ error: "Clothes not found!" });
+    }
+    results.map(function (currentCloth) {
+      return (currentCloth.img =
+        "http://localhost:3000/imgs/clothes_imgs/" + currentCloth.img);
+    });
+    const cloth = results[0];
+    // ex TAGLIE DEL VESTITO SPECIFICO
+    connection.query(sqlSizes, [id], (err, results) => {
+      if (err)
+        return res.status(500).json({
+          error: "Richiesta fallita!",
+        });
+      cloth.sizes = results;
+    });
+    // ex CATEGORIA DEL VESTITO SPECIFICO
+    connection.query(sqlCategory, [id], (err, results) => {
+      if (err)
+        return res.status(500).json({
+          error: "Richiesta fallita!",
+        });
+      cloth.category = results;
+      res.json(cloth);
+    });
+  });
 }
 
 export { index, show };
