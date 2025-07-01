53cde4c3655591ae37fe94400c3e1a0ecd769d2a
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-07-01 10:32:58 +0200

    change db

--- data/db.js
+++ data/db.js
@@ -1,9 +1,6 @@
 import mysql from "mysql2";
 import "dotenv/config";
-<<<<<<< HEAD
-=======
 
->>>>>>> 3ee0f30bd104e5e3e90728f81baf2a9eec525099
 const { APP_PASS } = process.env;
 
 const connection = mysql.createConnection({
