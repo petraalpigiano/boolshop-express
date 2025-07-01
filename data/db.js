f5492bfa27af78aab68b78f70ef702c7732e5d42
danirando <danielerando@tiscali.it>
2025-07-01 09:00:53 +0200

    add APP_PASS in .env and .env.example

--- data/db.js
+++ data/db.js
@@ -1,9 +1,12 @@
 import mysql from "mysql2";
+import "dotenv/config";
+
+const { APP_PASS } = process.env;
 
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root",
+  password: APP_PASS,
   database: "clothes",
   port: 3306,
 });
