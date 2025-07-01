f032478a1c2940452cdd309142d981b4448f6ed6
danirando <danielerando@tiscali.it>
2025-07-01 08:35:15 +0200

    fix password

--- data/db.js
+++ data/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2";
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "ventilatore",
+  password: "root",
   database: "clothes",
   port: 3306,
 });
