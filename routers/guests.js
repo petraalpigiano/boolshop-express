557bb3c5ac2a4fb237bc5bf9a61022cb2b3d4df2
petraalpigiano <petraalpigiano@gmail.com>
2025-07-08 12:38:30 +0200

    add validator for name

--- routers/guests.js
+++ routers/guests.js
@@ -1,13 +1,21 @@
 import express from "express";
 import { checkout } from "../controllers/clothController.js";
 import { validatePromoCode } from "../controllers/clothController.js";
+import checkoutValidator from "../middlewares/checkoutValidator.js";
+import { validationResult } from "express-validator";
 
 // ROUTER
 const router = express.Router();
 
 // VARIE ROUTE
 // INDEX/CLOTHES LIST
-router.post("/checkout", checkout);
+router.post("/checkout", checkoutValidator, (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  checkout(req, res, next);
+});
 router.post("/validate-code", validatePromoCode);
 
 export default router;
